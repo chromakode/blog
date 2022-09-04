85d2faa84cacf0d3eb98aa3adbfd7e56ac906e95
Max Goodhart <c@chromakode.com>
2022-09-04 16:16:01 -0700

    Add astro-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
-import sitemap from '@astrojs/sitemap'
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://chromakode.com',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), compress()],
   markdown: {
     shikiConfig: {
-      theme: 'github-light',
-    },
-  },
-})
+      theme: 'github-light'
+    }
+  }
+});
