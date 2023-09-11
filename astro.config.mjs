5e42fadbf0cdbb5708eac7c5135c86c202c69a40
Max Goodhart <c@chromakode.com>
2023-09-10 17:24:56 -0700

    Prefetch featured posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import preact from '@astrojs/preact'
+import prefetch from '@astrojs/prefetch'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://chromakode.com',
-  integrations: [mdx(), sitemap(), preact()],
+  integrations: [mdx(), sitemap(), preact(), prefetch()],
   markdown: {
     shikiConfig: {
       theme: 'github-light',
