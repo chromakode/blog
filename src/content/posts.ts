fb4bda3aff262b5f1d61d97c4f9d8b745d6df697
Max Goodhart <c@chromakode.com>
2023-09-10 20:21:49 -0700

    Work around Astro image deduplication bug
    withastro/astro#8047

--- src/content/posts.ts
+++ src/content/posts.ts
@@ -1,4 +1,5 @@
-import { getCollection } from 'astro:content'
+import { getImage } from 'astro:assets'
+import { getCollection, type CollectionEntry } from 'astro:content'
 
 export async function getPosts() {
   return (await getCollection('blog')).sort(
@@ -10,6 +11,14 @@ export async function getFeaturedPosts() {
   return (await getPosts())
     .slice(1)
     .filter((p) => p.data.feature && p.data.image && p.data.description)
+}
+
+export async function getPostImage(post: CollectionEntry<'blog'>) {
+  const src = post.data.contentImage ?? post.data.image
+  if (!src) {
+    return null
+  }
+  return await getImage({ src: src, height: 800, format: 'webp' })
 }
 
 export function postURL(slug: string) {
